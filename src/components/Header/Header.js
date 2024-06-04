0c6ffc8c69e549325efbfc66ebcac2593f88fc30
Denis Accardo <justdenys@gmail.com>
2024-06-04 15:43:08 +0200

    added bootstrap

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,15 @@ import { Link } from "react-router-dom";
 export const Header = () => {
     return (
         <div className="header">
-            <div className="wrap-logo-container">
-                <Link to={'/'}>
+            <div className="wrap-header">
+                <Link to={'/'} className="logo-link">
                     <img className="bnb-logo-img" src={process.env.PUBLIC_URL + '/images/logo/bnb-logo.png'}></img>
                 </Link>
+
+                <div className="place-name-container">
+                    <h1 className="place-name">B&B Favignana</h1>
+                </div>
             </div>
-            
-             
         </div>
     )
 }
